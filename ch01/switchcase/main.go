c32df1b4957860c97397887449aa77f02cb806d7
Tooru Takahashi <tooru.takahashi134@gmail.com>
2016-12-10 15:48:27 +0900

    Added switchcase/main.go.

---
+++ ch01/switchcase/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    len := len(os.Args)
+
+    fmt.Printf("%d\n", len);
+
+    switch {
+    case len < 3:
+        fmt.Println("len < 3")
+    case len < 2:
+        fmt.Println("len < 2")
+    default:
+        fmt.Println("default")
+        //fallthrough
+    }
+    fmt.Println("switch")
+}
