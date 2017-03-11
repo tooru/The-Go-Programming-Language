2b649237bc144612e84842deb74f421608d5f505
Tooru Takahashi <tooru.takahashi134@gmail.com>
2017-03-11 16:50:59 +0900

    Added nonameField.

---
+++ gopl/ch04/nonameField/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+    type A struct {
+        X int
+    }
+    type B struct {
+        X int
+        Y int
+    }
+    type C struct {
+        A
+        B
+        Y int
+    }
+
+    var c C
+
+    //c.X = 1 // CompileError ambiguous selector c.X
+    c.A.X = 1
+    c.B.X = 1
+    c.Y = 1
+}
