ee797e256fd6881b2aef7c256e53085e1cda46f4
Tooru Takahashi <tooru.takahashi134@gmail.com>
2017-03-11 14:03:55 +0900

    Added recursiveType.

---
+++ gopl/ch04/recursiveType/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    type S []S
+    var s = make(S, 1)
+    s[0] = s
+
+    fmt.Println(len(s))
+    fmt.Printf("%p %p %p\n", s, s[0], s[0][0])
+    // fmt.Println(s) // stack overflow
+}
