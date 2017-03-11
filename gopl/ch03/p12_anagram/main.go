8dfc41901b80a33044837a9a475087f0ea0ad368
Tooru Takahashi <tooru.takahashi134@gmail.com>
2017-03-11 15:34:28 +0900

    Added ch03/p12_anagram.

---
+++ gopl/ch03/p12_anagram/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func isAnagram(a, b string) bool {
+    if len(a) != len(b) {
+        return false
+    }
+
+    for i, j := 0, len(a)-1; i <= j; i,j = i+1,j-1 {
+        if a[i] != b[j] {
+            return false
+        }
+    }
+    return true
+}
+
+func main() {
+    a := os.Args[1]
+    b := os.Args[2]
+
+    fmt.Printf("%s %s => %v\n", a, b, isAnagram(a, b))
+}
