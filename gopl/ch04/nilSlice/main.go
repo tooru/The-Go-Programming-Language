c7ce0ee9b937f79ba496f085c129dc9279be812d
Tooru Takahashi <tooru.takahashi134@gmail.com>
2017-03-11 15:38:56 +0900

    Modified nilSlice.

--- gopl/ch04/nilSlice/main.go
+++ gopl/ch04/nilSlice/main.go
@@ -11,12 +11,26 @@ func main() {
 
     var s []int
 
-    fmt.Printf("%#v\n", s)
-    fmt.Printf("%#v\n", s[:])
-    fmt.Printf("%#v\n", s[:0])
+    fmt.Printf("%#v %v\n", s, s == nil)
+    s = s[:]
+    fmt.Printf("%#v %v\n", s, s == nil)
+    s = s[:0]
+    fmt.Printf("%#v %v\n", s, s == nil)
 
     s = make([]int, 0)
-    fmt.Printf("%#v\n", s)
-    fmt.Printf("%#v\n", s[:])
-    fmt.Printf("%#v\n", s[:0])
+
+    fmt.Printf("%#v %v\n", s, s == nil)
+    s = s[:]
+    fmt.Printf("%#v %v\n", s, s == nil)
+    s = s[:0]
+    fmt.Printf("%#v %v\n", s, s == nil)
+
+    // go v1.7.3
+    // <nil>
+    // []int(nil) true
+    // []int(nil) true
+    // []int(nil) true
+    // []int{} false
+    // []int{} false
+    // []int{} false
 }
