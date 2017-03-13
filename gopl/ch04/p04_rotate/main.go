fccd8ceb67ada030b5b8e5e4ecaf666126906b2f
Tooru Takahashi <tooru.takahashi134@gmail.com>
2017-03-13 23:38:52 +0900

    Added main.go and main_test.go.

---
+++ gopl/ch04/p04_rotate/main.go
@@ -0,0 +1,14 @@
+package p04_rotate
+
+// 2-pass?
+
+func Rotate(s []int, r int) {
+    t := make([]int, len(s))
+
+    for i := 0; i < len(s); i++ {
+        t[(i+r)%len(s)] = s[i]
+    }
+    for i := 0; i < len(s); i++ {
+        s[i] = t[i]
+    }
+}
---
+++ gopl/ch04/p04_rotate/main_test.go
@@ -0,0 +1,39 @@
+package p04_rotate
+
+import (
+    "testing"
+)
+
+func assertEquals(t *testing.T, x, y []int) {
+    if len(x) != len(y) {
+        t.Errorf("%v %v", x, y)
+        return
+    }
+    for i := 0; i < len(x); i++ {
+        if x[i] != y[i] {
+            t.Errorf("%v %v", x, y)
+            return
+        }
+    }
+}
+
+func rotate(s []int, r int) []int {
+    Rotate(s, r)
+
+    return s
+}
+
+func TestRotate(t *testing.T) {
+    assertEquals(t, nil, rotate(nil, 0))
+
+    assertEquals(t, []int{0}, rotate([]int{0}, 0))
+    assertEquals(t, []int{0}, rotate([]int{0}, 1))
+
+    assertEquals(t, []int{0, 1}, rotate([]int{0, 1}, 0))
+    assertEquals(t, []int{1, 0}, rotate([]int{0, 1}, 1))
+    assertEquals(t, []int{0, 1}, rotate([]int{0, 1}, 2))
+
+    assertEquals(t, []int{0, 1, 2}, rotate([]int{0, 1, 2}, 0))
+    assertEquals(t, []int{2, 0, 1}, rotate([]int{0, 1, 2}, 1))
+    assertEquals(t, []int{1, 2, 0}, rotate([]int{0, 1, 2}, 2))
+}
